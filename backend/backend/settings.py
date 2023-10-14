57d88bd5ebb15dcc60497d8befed4171b843d331
Sergei Yakovlev <lol0707h@gmail.com>
2023-10-14 15:29:53 +0500

    v1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['db', '51.250.110.17', '127.0.0.1', '0.0.0.0', 'localhost', 'takmaster.hopto.org']
 
 
 # Application definition
